63b8496d6638ed6f03078835fa4f68bb39ee85a1
Jad Abou Zeid <jad.abzd01@gmail.com>
2021-09-23 12:28:03 +0300

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,27 @@
 const express = require("express"); //import express
 const notes = require("./data/notes"); //import notes
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const { errorHandler, notFound } = require("./middlewares/errorMiddleware");
 
 const app = express(); //creates an object of express
-
 dotenv.config();
+connectDB();
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
+//app.get("/", (req, res) => {
+//  res.send("API is running...");
+//});
 
-app.get("/api/notes", (req, res) => {
-  res.json(notes);
-});
+//app.get("/api/notes", (req, res) => {
+//  res.json(notes);
+//});
 
-app.get("/api/notes/:id", (req, res) => {
-  const note = notes.find((n) => n._id === req.params.id);
-  res.send(note);
-});
+app.use("/api/users", userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
